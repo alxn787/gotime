903c8c72411e709aba31d08af4c6ca6b26be9f35
alxn787 <alenabraham787@gmail.com>
2025-07-29 00:23:27 +0530

    gooooo

--- main.go
+++ main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	
+
+	c := make(chan int)
+
+	go func () {
+		firstSum := <-c
+		fmt.Println(firstSum)
+		secondSum := firstSum + 10
+		c<-secondSum
+	}()
+
+	c <- 100
+	fmt.Println(<-c)
+
 }
